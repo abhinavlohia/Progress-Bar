84bdd0d5b60b4f21586e16ee9155c3ad7a2d349c
abhinavlohia <59476093+abhinavlohia@users.noreply.github.com>
2025-01-25 16:02:42 +0000

    First Attempt Done

--- src/ProgressBar.jsx
+++ src/ProgressBar.jsx
@@ -7,15 +7,22 @@ export default function ProgressBar() {
     if (currPercentage < 100) {
       setTimeout(() => {
         setCurrPercentage((prev) => prev + 1);
+        const progress = document.getElementsByClassName("progress")[0];
+        progress.style.width = `${currPercentage + 1}%`;
       }, [50]);
+    }
+
+    if (currPercentage > 50) {
+      const progressText = document.getElementsByClassName("percentageText")[0];
+      progressText.style.color = "#fff";
     }
   }, [currPercentage]);
 
   return (
     <div className="progressBarContainer">
       <span className="heading">Progress Bar</span>
-      <div className=""></div>
       <div className="progressBar">
+        <div className="progress"></div>
         <div className="percentageText">{currPercentage}%</div>
       </div>
     </div>
