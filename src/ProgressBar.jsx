44d8949a78ccbfb2cee9f638cc36e93e22a9b3ee
abhinavlohia <59476093+abhinavlohia@users.noreply.github.com>
2025-01-26 11:32:17 +0000

    Second Attempt

--- src/ProgressBar.jsx
+++ src/ProgressBar.jsx
@@ -1,29 +1,41 @@
 import { useEffect, useState } from "react";
 
-export default function ProgressBar() {
+export default function ProgressBar({ duration = 50 }) {
   const [currPercentage, setCurrPercentage] = useState(0);
 
   useEffect(() => {
     if (currPercentage < 100) {
-      setTimeout(() => {
+      const timer = setTimeout(() => {
         setCurrPercentage((prev) => prev + 1);
-        const progress = document.getElementsByClassName("progress")[0];
-        progress.style.width = `${currPercentage + 1}%`;
-      }, [100]);
-    }
+      }, duration);
 
-    if (currPercentage > 50) {
-      const progressText = document.getElementsByClassName("percentageText")[0];
-      progressText.style.color = "#fff";
+      return () => clearTimeout(timer);
     }
   }, [currPercentage]);
+
+  const progressStyle = {
+    width: `${currPercentage}%`,
+    background: "#2c8558",
+    height: "100%",
+    borderRadius: "8px",
+    transition: "width 0.3s ease",
+  };
+
+  const percentageTextStyle = {
+    position: "absolute",
+    left: "50%",
+    color: "#000",
+    fontSize: "14px",
+    fontWeight: "600",
+    color: currPercentage > 55 ? "#fff" : "#000",
+  };
 
   return (
     <div className="progressBarContainer">
       <span className="heading">Progress Bar</span>
       <div className="progressBar">
-        <div className="progress"></div>
-        <div className="percentageText">{currPercentage}%</div>
+        <div style={progressStyle}></div>
+        <div style={percentageTextStyle}>{currPercentage}%</div>
       </div>
     </div>
   );
